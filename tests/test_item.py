514fc0a5c3eb69ceb0171ec0c0931c2ac4667b51
Alexandr Lebedev <alexandr.lebedev.sp@gmail.com>
2023-09-07 21:24:50 +0300

    all work

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+item1 = Item('tel', 10, 5)
+Item.pay_rate = 5
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 50
+
+
+def test_apply_discount():
+    item1.apply_discount()
+    assert item1.price == 50
