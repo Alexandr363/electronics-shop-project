7186b65a0ae11be24c159e0540afeffd69ef10f2
Alexandr Lebedev <alexandr.lebedev.sp@gmail.com>
2023-09-10 11:35:21 +0300

    homework-2 ready

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине
@@ -9,11 +12,55 @@ class Item:
         """
         Создание экземпляра класса item
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         self.all.append(self)
+
+    @classmethod
+    def instantiate_from_csv(cls, filename) -> None:
+        """
+        Открывает файл csv, создаёт экземпляры классы Item на основании данных
+        из открытого файла
+        """
+        Item.all = []
+        with open('../src/items.csv', newline='',
+                  encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = str(row['name'])
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                cls(name, price, quantity)
+
+    @property
+    def name(self):
+        """
+        Геттер атрибута __name, возвращает значение __name
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        """
+        Сеттер атрибута __name, проверяет длину переданного имени экземпляра
+        и обрезает её до 10 символов (если она больше)
+        """
+        if len(name) <= 10:
+            self.__name = name
+        if len(name) >= 10:
+            name = name[:10]
+            self.__name = name
+
+    @staticmethod
+    def string_to_number(data: str) -> int:
+        """
+        Принимает строку и возвращает значение int
+        """
+        if len(data) > 1:
+            return int(data.split('.')[0])
+        return int(data)
 
     def calculate_total_price(self) -> float:
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -13,3 +13,22 @@ def test_calculate_total_price():
 def test_apply_discount():
     phone.apply_discount()
     assert phone.price == 50
+
+
+def test_string_to_number():
+    assert Item.string_to_number('357.19') == 357
+    assert Item.string_to_number('9') == 9
+
+
+def test_name_setter():
+    test = Item('Пылесос', 500, 1)
+    test.name = 'Утюг'
+    assert test.name == 'Утюг'
+    test.name = 'Мультиварка'
+    assert test.name == 'Мультиварк'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('/Users/mac/Dev/electronics-shop-project'
+                              '/src/items.csv')
+    assert len(Item.all) == 5
